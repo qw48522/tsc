04059b8f7975898a7a7d97b013f897225407b2b4
qw48522 <741593477@qq.com>
2017-05-07 12:47:42 +0800

    Update tsc.c

--- tsc/tsc.c
+++ tsc/tsc.c
@@ -9,11 +9,11 @@ typedef struct date
 void getdate(pdate a)
 {
     printf("Year:\n");
-	scanf("%d",&a->year);
+    scanf("%d",&a->year);
     printf("Month:\n");
-	scanf("%d",&a->month);
-	printf("Day:\n");
-	scanf("%d",&a->day);
+    scanf("%d",&a->month);
+    printf("Day:\n");
+    scanf("%d",&a->day);
 }
 int bj(pdate a,pdate b)
 {
@@ -23,22 +23,22 @@ int bj(pdate a,pdate b)
 		return 0;
 	else
 	{
-		if(a->month>b->month)
-		    return 1;
-	    else if(a->month<b->month)
-		    return 0;
-	    else
-		{
-            if(a->day>b->day)
-		         return 1;
+	        if(a->month>b->month)
+		        return 1;
+	        else if(a->month<b->month)
+		        return 0;
 	        else
-				return 0;
-		}
+	        {
+                        if(a->day>b->day)
+		                 return 1;
+	                else
+		                 return 0;
+	        }
 	}
 }
 int day(pdate a)
 {
-    int i;
+        int i;
 	int f=0;
 	for(i=a->month;i>=1;i--)
 	{
@@ -46,44 +46,44 @@ int day(pdate a)
 			f+=a->day;
 		else
 		{
-            if(i==1||i==3||i==5||i==7||i==8||i==10||i==12)
-			    f+=31;
-		    else if(i==2)
+                        if(i==1||i==3||i==5||i==7||i==8||i==10||i==12)
+			        f+=31;
+		        else if(i==2)
 			{
-			    if((a->year%4==0&&a->year%100!=0)||a->year%400==0)
-			        f+=29;
-		     	else
-				    f+=28;
+			        if((a->year%4==0&&a->year%100!=0)||a->year%400==0)
+			                f+=29;
+		     	        else
+				        f+=28;
 			}
-		    else
-			    f+=30;
+		        else
+			        f+=30;
 		}
-    }
-	return f;
+        }
+        return f;
 }
 int tsc(pdate a,pdate b)
 {
-    int i;
+        int i;
 	int f1=0,f2=0,f3=0;
 	for(i=b->year;i<a->year;i++)
 	{
 		if((i%4==0&&i%100!=0)||i%400==0)
-             f1+=366;
+                        f1+=366;
 		else
-			 f1+=365;
+			f1+=365;
 	}
-    f2=day(a);
+        f2=day(a);
 	f3=day(b);
 	return f1+f2-f3;
 }
 void main()
 {
 	pdate a,b;
-    a=(pdate)malloc(sizeof(struct date));
-    b=(pdate)malloc(sizeof(struct date));
-    getdate(a);
+        a=(pdate)malloc(sizeof(struct date));
+        b=(pdate)malloc(sizeof(struct date));
+        getdate(a);
 	getdate(b);
 	int i;
 	i=(bj(a,b)?tsc(a,b):tsc(b,a));
-    printf("tsc :%d\n",i);
+        printf("tsc :%d\n",i);
 }
